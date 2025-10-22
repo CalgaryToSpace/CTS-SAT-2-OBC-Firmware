824e56568203a121f6e9a63395acbc5870958381
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-10-21 20:13:07 -0600

    test: change test to something slightly less contrived

--- cts2_obc_logic/src/lib.rs
+++ cts2_obc_logic/src/lib.rs
@@ -6,8 +6,8 @@ extern crate std;
 use cts2_obc_telecommands::Telecommand; // Sample include; use later.
 
 
-pub fn return_42() -> u32 {
-    42
+pub fn multiply_by_2(i: u32) -> u32 {
+    i * 2
 }
 
 
@@ -17,6 +17,7 @@ mod tests {
 
     #[test]
     fn test_placeholder() {
-        assert_eq!(return_42(), 42);
+        assert_eq!(multiply_by_2(21), 42);
+        assert_eq!(multiply_by_2(0), 0);
     }
 }
