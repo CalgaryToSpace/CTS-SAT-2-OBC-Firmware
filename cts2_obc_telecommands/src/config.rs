437b1e0e400a00ee4644ff9bf429a259ab60a8d7
c-khoo <chloe.hc.khoo@gmail.com>
2026-02-14 12:26:18 -0700

    Implement getter and setter for an IndexMap of u32 configuration variables

--- cts2_obc_telecommands/src/config.rs
+++ cts2_obc_telecommands/src/config.rs
@@ -1,24 +1,37 @@
-/**
- * - Enum of all configuration variable names.
- * - Struct that we create a global static singleton that contains all variable names.
- * - Create a getter and a setter telecommand (getter arg: name of variable, 
- *   setter arg: name of variable, value)
- * - Add configuration variable: heartbeat in ms (for testing to start)
- * - Add configuration variable: config_demo_variable1
- */
-
 #![cfg_attr(not(test), no_std)]
 
 #[cfg(test)]
 extern crate std;
 
-#[derive(Debug)]
+extern crate cortex_m;
 
+use core::cell::RefCell;
+use cortex_m::interrupt::Mutex;
+use cortex_m::interrupt::free as critical_section;
+use heapless::index_map::FnvIndexMap;
 
+#[derive(Debug, Eq, PartialEq, Hash)]
 // Enum of all configuration variable names
-enum ConfigVariable {
-    CONFIG_int_demo_var1,
-    CONFIG_int_demo_var2,
-    TASK_heartbeat_ms,
+#[allow(non_camel_case_types)]
+pub enum ConfigVariable {
+    heartbeat_ms,
+    config_demo_variable1,
     // TODO: Add more configuration variables here
+}
+
+static CONFIG_U32_VARIABLES: Mutex<RefCell<FnvIndexMap<ConfigVariable, u32, 2>>> =
+    Mutex::new(RefCell::new(FnvIndexMap::new()));
+
+pub fn config_set_u32_variable(var_name: ConfigVariable, new_value: u32) {
+    critical_section(|cs| {
+        if let Some(value) = CONFIG_U32_VARIABLES.borrow(cs).borrow_mut().get_mut(&var_name) {
+            *value = new_value;
+        }
+    });
+}
+
+pub fn config_get_u32_variable(var_name: ConfigVariable) -> Option<u32> {
+    critical_section(|cs| {
+        CONFIG_U32_VARIABLES.borrow(cs).borrow().get(&var_name).copied()
+    })
 }
