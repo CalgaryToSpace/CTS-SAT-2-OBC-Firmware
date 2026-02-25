b42a7399a6c0a8a599be9a7fcb5b250358324239
c-khoo <chloe.hc.khoo@gmail.com>
2026-02-24 18:41:28 -0700

    Write function to insert configuration variables into IndexMap, modify setter to return Result

--- cts2_obc_telecommands/src/config.rs
+++ cts2_obc_telecommands/src/config.rs
@@ -9,9 +9,10 @@ use core::cell::RefCell;
 use cortex_m::interrupt::Mutex;
 use cortex_m::interrupt::free as critical_section;
 use heapless::index_map::FnvIndexMap;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Eq, PartialEq, Hash)]
 // Enum of all configuration variable names
+#[derive(Debug, Eq, PartialEq, Hash, Deserialize, Serialize)]
 #[allow(non_camel_case_types)]
 pub enum ConfigVariable {
     heartbeat_ms,
@@ -22,12 +23,32 @@ pub enum ConfigVariable {
 static CONFIG_U32_VARIABLES: Mutex<RefCell<FnvIndexMap<ConfigVariable, u32, 2>>> =
     Mutex::new(RefCell::new(FnvIndexMap::new()));
 
-pub fn config_set_u32_variable(var_name: ConfigVariable, new_value: u32) {
+// Attempt to add configuration variables (temporary solution, but what is a better way?)
+
+pub fn config_all_u32() {
+    critical_section(|cs| {
+        let mut config_u32 = CONFIG_U32_VARIABLES.borrow(cs).borrow_mut();
+        config_u32.insert(ConfigVariable::heartbeat_ms, 500).unwrap();
+        config_u32.insert(ConfigVariable::config_demo_variable1, 12345).unwrap();
+    });
+}
+
+// I think I will need to modify this to be more in line with how telecommands are set up.
+// The telecommand versions should CALL these getters and setters?
+// Worry about having a struct (like in DemoCommandWithArgumentsArgs) for the telecommand, not here
+// Error handling/how to communicate that an error occured? (like return value in C?)
+
+
+pub fn config_set_u32_variable(var_name: ConfigVariable, new_value: u32) -> Result <(), ()> {    
+    // Potential for error handling here?
+    
     critical_section(|cs| {
         if let Some(value) = CONFIG_U32_VARIABLES.borrow(cs).borrow_mut().get_mut(&var_name) {
             *value = new_value;
         }
     });
+
+    Ok(())
 }
 
 pub fn config_get_u32_variable(var_name: ConfigVariable) -> Option<u32> {
