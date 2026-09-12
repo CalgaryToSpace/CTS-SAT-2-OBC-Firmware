d2b1bcfc23c65416864aade910da3ff942f60cfa
c-khoo <chloe.hc.khoo@gmail.com>
2026-09-12 12:26:51 -0600

    feat: Update OBC info telecommand to include UNIX build timestamp (#48) * Timestamp tracks the first build after a new commit is made.

--- cts2_obc_firmware/build.rs
+++ cts2_obc_firmware/build.rs
@@ -1,9 +1,12 @@
 use std::process::Command;
+use std::time::SystemTime;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
     // rerun build file if HEAD changes (ie. there are new commits)
-    println!("cargo:rerun-if-changed=.git/HEAD");
-    println!("cargo:rerun-if-changed=.git/refs/heads");
+    println!("cargo:rerun-if-changed=../.git/HEAD");
+    println!("cargo:rerun-if-changed=../.git/refs/heads");
 
     let output = Command::new("git").args(["rev-parse", "HEAD"]).output();
 
@@ -14,6 +17,12 @@ fn main() {
         _ => "unknown".to_string(),
     };
 
-    // embed the git commit hash as an environment variable to be read in by telecommand
+    let build_timestamp: i64 = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(duration) => duration.as_secs() as i64,
+        Err(e) => -(e.duration().as_secs() as i64),
+    };
+
+    // embed the git commit hash and build timestamp as environment variables
     println!("cargo:rustc-env=COMMIT_HASH={}", commit_hash);
+    println!("cargo:rustc-env=BUILD_TIMESTAMP={}", build_timestamp);
 }
--- cts2_obc_firmware/src/telecommand_implementation.rs
+++ cts2_obc_firmware/src/telecommand_implementation.rs
@@ -44,10 +44,12 @@ pub fn set_config_variable(
 
 pub fn get_obc_info() -> Result<(), ExecuteCommandErr> {
     let commit_hash = env!("COMMIT_HASH");
+    let build_timestamp = env!("BUILD_TIMESTAMP");
 
     // May need to define a larger buffer once more info is added - git commit hash is 40 characters long
     let mut buffer = heapless::String::<128>::new();
     let _ = write!(buffer, "Commit Hash: {:?}\r\n", commit_hash);
+    let _ = write!(buffer, "Build Timestamp (UNIX): {:?}\r\n", build_timestamp);
 
     send_umbilical_uart(buffer.as_bytes());
     Ok(())
